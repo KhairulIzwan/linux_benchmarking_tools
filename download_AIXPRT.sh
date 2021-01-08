73632c9ad044be4b1bd65f4920c879c0c8b3859f
KhairulIzwan <wansnap@gmail.com>
2021-01-08 22:29:53 +0800

    Updated on 01-07-2021-07:16:59

--- download_AIXPRT.sh
+++ download_AIXPRT.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 # -------------------------------------------------
+
 # Usage: Installing AIXPRT on Linux (Ubuntu)
 # Author: Khairul Izwan Bin Kamsani (wansnap@gmail.com)
 
@@ -8,6 +10,7 @@
 # https://unix.stackexchange.com/questions/24721/how-to-compare-to-floating-point-number-in-a-shell-script
 # https://www.tecmint.com/check-linux-cpu-information/
 # https://linuxize.com/post/bash-exit/
+# https://linuxize.com/post/bash-case-statement/
 
 # -------------------------------------------------
 
@@ -28,7 +31,7 @@ echo -e "Welcome, $(whoami)!"
 
 # System checking
 # TODO: Checking others (min) system requirements: OS, CPU, GPU, VPU
-if [ ${Release##*.} -le 18 ]
+if [ ${Release##*.} -ge 18 ]
 then
 	echo -e "Your machine meet the minimum system requirements:"
 	echo -e "$Distributor_ID"
@@ -51,26 +54,44 @@ echo -e "[2]  TensorFlow"
 echo -e "[3]  NVIDIA TensorRT"
 echo -e "[4]  MXNet"
 echo -e "[5]  All"
-echo -e "Which AIXPRT test packages to be installed? [1/2/3/4/5]:"
+echo -n "Which AIXPRT test packages to be installed? [1/2/3/4/5]:"
 
 read option
 
-if [ $option -le 18 ]
-then
-	echo -e "Your machine meet the minimum system requirements:"
-	echo -e "$Distributor_ID"
-	echo -e "$Description"
-	echo -e "$Release"
-	echo -a "$Codename"
-else
-	echo -e "Your machine NOT meet the minimum system requirements:"
-	echo -e "$Distributor_ID"
-	echo -e "$Description"
-	echo -e "$Release"
-	echo -e "$Codename"
-	
-	exit
-fi
-## Downloading the AIXPRT
-#echo -e "Downloading the AIXPRT..."
-#wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_OpenVINO_Ubuntu.zip
+echo -n "AIXPRT test packages to be installed is "
+
+case $option in
+
+	1)
+		echo -e "[1] Intel OpenVINO"
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_OpenVINO_Ubuntu.zip
+		;;
+		
+	2)
+		echo -e "[2] TensorFlow"
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_Tensorflow_Ubuntu.zip
+		;;
+		
+	3)
+		echo -e "[3] NVIDIA TensorRT"
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_TensorRT_Ubuntu.zip
+		;;
+		
+	4)
+		echo -e "[4] MXNet"
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_MXNet_Ubuntu.zip
+		;;
+		
+	5)
+		echo -e "[5] All"
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_OpenVINO_Ubuntu.zip
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_Tensorflow_Ubuntu.zip
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_TensorRT_Ubuntu.zip
+		wget https://www.principledtechnologies.com/benchmarkxprt/aixprt/2019/AIXPRT_1.0_MXNet_Ubuntu.zip
+		;;
+		
+	*)
+		echo -n "Not listed, Installation halt!"
+		exit
+		;;
+esac
